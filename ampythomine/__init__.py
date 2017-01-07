f28addc0353d0ae39284dc28281ac63cc73991dd
Your Name <you@example.com>
2017-01-07 16:35:34 +0700

    umongo backend, example

--- ampythomine/__init__.py
+++ ampythomine/__init__.py
@@ -1,11 +1,3 @@
-from setuptools import setup
+from .core import Application
 
-setup(name='ampythomine',
-      version='0.1',
-      description='Web framework based on aiohttp',
-      url='http://github.com/patlach42/ampythomine',
-      author='Fedotov Maxim',
-      author_email='f42.dobro@gmail.com',
-      license='MIT',
-      packages=['ampythomine'],
-      zip_safe=False)
+__all__ = (Application)
