09f359eeecda3f63eeed27d653cca3532a6fc62e
Your Name <you@example.com>
2017-01-07 11:35:38 +0700

    Added readme

---
+++ ampythomine/__init__.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='ampythomine',
+      version='0.1',
+      description='Web framework based on aiohttp',
+      url='http://github.com/patlach42/ampythomine',
+      author='Fedotov Maxim',
+      author_email='f42.dobro@gmail.com',
+      license='MIT',
+      packages=['ampythomine'],
+      zip_safe=False)
