487b73f68b5161d736079ce505688aab0c479bca
Your Name <you@example.com>
2017-01-08 20:00:56 +0700

    setup script

--- setup.py
+++ setup.py
@@ -1,4 +1,4 @@
-from setuptools import setup
+from setuptools import setup, find_packages
 
 setup(name='ampythomine',
       version='0.1',
@@ -7,5 +7,5 @@ setup(name='ampythomine',
       author='Fedotov Maxim',
       author_email='f42.dobro@gmail.com',
       license='MIT',
-      packages=['ampythomine', 'ampythomine.db', 'ampythomine.auth'],
+      packages=find_packages(),
       zip_safe=False)
